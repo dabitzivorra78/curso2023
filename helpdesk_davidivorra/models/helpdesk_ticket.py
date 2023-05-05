57ada0d92d41d85c8a4da6835d69442e3a5fc344
dabitzivorra78 <dabitz778@gmail.com>
2023-05-05 15:57:21 +0000

    task-01-02 - creación de vista

--- helpdesk_davidivorra/models/helpdesk_ticket.py
+++ helpdesk_davidivorra/models/helpdesk_ticket.py
@@ -11,10 +11,19 @@ class HelpdeskTicket(models.Model):
         help="Resume brevemente en el título la incidencia"
     )
     
+    # Secuencia
+    sequence = fields.Integer(
+            default=10,
+            help="Contador para el orden de las incidencias"
+    )
     # Descripción
     description = fields.Text(
         help="Detalla lo mejor posible la incidencia y como se produce.",
-        default="Versión a la que afecta:     Modulo:    Pasos para el error:    Modulos personalizados:"
+        default="""Versión a la que afecta:     
+    Modulo:    
+    Pasos para el error:    
+    Modulos personalizados:
+        """
     )
     
     # Fecha
@@ -25,7 +34,9 @@ class HelpdeskTicket(models.Model):
         string='Limit Date &Time')
    
     # Asignado (Verdadero o Falso)
-    assigned = fields.Boolean()
+    assigned = fields.Boolean(
+        readonly=True,
+    )
     user_id = fields.Many2one(
         comodel_name='res.users',
         string="Assigned to"
@@ -33,3 +44,16 @@ class HelpdeskTicket(models.Model):
     # Acciones a realizar (Html)
     actions_todo = fields.Html()
 
+    #Añadir el campo Estado [Nuevo, Asignado, En proceso, Pendiente, Resuelto, Cancelado], que por defecto sea Nuevo
+    state = fields.Selection(
+        selection=[
+            ('new', 'New'),
+            ('assigned', 'Assigned'),
+            ('in_progress', 'In Process'),
+            ('pendind', 'Pending'),
+            ('resolved', 'Resolved'),
+            ('canceled', 'Canceled'),
+        ],
+        default='new',
+    )
+    #El campo Asignado hacer que sea solo de lectura
