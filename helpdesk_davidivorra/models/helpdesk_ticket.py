78d721377e0195fdaeee3ab17a02f5d9b3976a14
dabitzivorra78 <dabitz778@gmail.com>
2023-05-11 16:58:09 +0000

    añadido kanban y color

--- helpdesk_davidivorra/models/helpdesk_ticket.py
+++ helpdesk_davidivorra/models/helpdesk_ticket.py
@@ -72,3 +72,5 @@ class HelpdeskTicket(models.Model):
     def set_actions_as_done(self):
         self.ensure_one()
         self.action_ids.set_done()
+
+    color = fields.Integer('Color Index', default=0)
