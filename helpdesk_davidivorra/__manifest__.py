ce3e75c6f0a3090f214d819134d63b8d3bf49690
dabitzivorra78 <dabitz778@gmail.com>
2023-05-16 16:01:37 +0000

    task 03-01

--- helpdesk_davidivorra/__manifest__.py
+++ helpdesk_davidivorra/__manifest__.py
@@ -17,8 +17,9 @@
     "data": [
         "security/helpdesk_security.xml",
         "security/ir.model.access.csv",
+        "views/helpdesk_ticket_action_views.xml",
         "views/helpdesk_ticket_views.xml",
         "views/helpdesk_ticket_tag_views.xml",
-        "views/helpdesk_ticket_action_views.xml"
+        "data/helpdesk_cron.xml",
     ],
 }
--- helpdesk_davidivorra/models/helpdesk_ticket.py
+++ helpdesk_davidivorra/models/helpdesk_ticket.py
@@ -1,5 +1,6 @@
 from odoo import api, Command,fields, models
 from odoo.exceptions import UserError 
+from datetime import timedelta
 
 class HelpdeskTicket(models.Model):
     _name = 'helpdesk.ticket'
@@ -27,13 +28,42 @@ class HelpdeskTicket(models.Model):
         """
     )
     
+    @api.model
+    def _get_default_date(self):
+        return fields.Date.today()
+    
     # Fecha
-    date = fields.Date()
+    date = fields.Date(
+        default=_get_default_date,
+    )
     
     # Fecha y hora limite
     date_limit = fields.Datetime(
-        string='Limit Date &Time')
-   
+        string='Limit Date & Time',
+        #compute='_compute_date_limit',
+        #inverse='_inverse_date_limit',
+        #store=True
+        )
+    
+    #@api.depends('date')
+    #def _compute_date_limit(self):
+    #    for record in self:
+    #        if record.date:
+    #            record.date_limit = record.date + timedelta(days=1)
+    #        else:
+    #            record.date_limit = False
+    #def _inverse_date_limit(self):
+    #    pass
+    
+    #Añadir un onchange para que al indicar la fecha ponga como fecha de vencimiento un día mas
+
+    @api.onchange('date')
+    def _onchange_date(self):
+            if self.date:
+                self.date_limit = self.date + timedelta(days=1)
+            else:
+                 self.date_limit = False
+
     # Asignado (Verdadero o Falso)
     assigned = fields.Boolean(
         readonly=True,
@@ -81,6 +111,14 @@ class HelpdeskTicket(models.Model):
     )
     amount_time = fields.Float(
         string="Amount of time")
+    
+    #- Añadir una restricción para hacer que el campo time no sea menor que 0
+
+    @api.constrains('amount_time')
+    def _chek_amount_time(self):
+        for record in self:
+            if record.amount_time <0:
+                raise UserError( ("The amount of time can't be negative"))
 
         #Hacer que el campo assigned sea calculado, hacer que se pueda buscar con el atributo search y 
         #hacer que se pueda modificar de forma que si lo marco se actualice el usuario con el usuario conectado y 
@@ -125,11 +163,27 @@ class HelpdeskTicket(models.Model):
     #crear un campo nombre de etiqueta, y hacer un botón que cree la nueva etiqueta con ese nombre y lo asocie al ticket.
     tag_name = fields.Char()
     
+    #Modificar el botón de crear una etiqueta en el formulario de ticket para que abra una acción nueva, 
+    #pasando por contexto el valor del nombre y la relación con el ticket.
     def create_tag(self):
         self.ensure_one()
         #self.write({'tag_ids' : [(0,0,{'name': self.tag_name})]})     
         #self.write({'tag_ids' : [Command.create({'name': self.tag_name})]})
-        self.tag_ids = [Command.create({'name': self.tag_name})]
+       #action = self.env["ir.actions.actions"]._for_xml_id("helpdesk_davidivorra.helpdesk_ticket_tag_action")
+        action ={
+            'name': 'Helpdesk Tickets Tags',
+            'type': 'ir.actions.act_window',
+            'res_model': 'helpdesk.ticket.tag',
+        }     
+        action['context'] = {
+            'default_name': self.tag_name,
+            'default_ticket_id': self.id,    
+        }
+        action['view_mode'] = 'form'
+        action['binding_view_types'] = 'form'
+        action['target'] = 'new'
+        return action
+
     def clear_tags(self):
         self.ensure_one()
         tag_ids = self.env['helpdesk.ticket.tag'].search([('name', '=', 'otra')])
@@ -138,8 +192,26 @@ class HelpdeskTicket(models.Model):
         #    (5,0,0),
         #    (6,0,tag_ids.ids)]})  
 
-        import pdb; pdb.set_trace()
-        self.tag_ids = [
+        self.tag_ids =[
             Command.clear(),
             Command.set(tag_ids.ids)]
+        
+    def get_related_actions(self):
+        self.ensure_one()
+        #action = self.env["ir.actions.actions"]._for_xml_id("helpdesk_davidivorra.helpdesk_ticket_action_related_action")
+        #return action
+        action = self.env["ir.actions.actions"]._for_xml_id("helpdesk_davidivorra.helpdesk_ticket_action_action")
+        action['domain'] = [('ticket_id','=',self.id)]
+        action['context'] = {'default_ticket_id': self.id}
+        return action
+        
+    def get_assigned(self):
+        self.ensure_one()
+        self.state ='assigned'
+        self.user_id = self.env.user
+
+       # import pdb; pdb.set_trace()
+       # self.tag_ids = [
+       #     Command.clear(),
+       #     Command.set(tag_ids.ids)]
 
